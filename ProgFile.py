743da82b2f2b8044a012452d82d172648dd39f34
DeynekoVladimir55555 <vladimir.deyneko@yandex.com>
2023-11-22 13:06:51 +0300

    delete prog files

--- ProgFile.py
+++
@@ -1,4 +0,0 @@
-resoult_list_for_answer_of_user = []
-a = 730
-if a % 468 == 0:
-    resoult_list_for_answer_of_user.append(a)
--- StatisticsWindows.py
+++ StatisticsWindows.py
@@ -28,27 +28,6 @@ class StatisticsQuestionWindow(QMainWindow):
     def close_window(self):
         self.close()
 
-class StatisticsProgrammingWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('uis\StatisticsWindow.ui', self)
-        self.setWindowTitle('Статистика')
-        self.closeButton.clicked.connect(self.close_window)
-        self.updateButton.clicked.connect(self.update)
-        #Показывание окна поверх главного
-        self.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
-        #Передвижение на центр экрана
-        MoveFunction.move_to_senter(self)
-
-    def update(self):
-        #Получение статистики и её вывод
-        a = DataBaseFile.statistics('ProgAnswers')
-        self.trueLabel.setText('Решено верно:   ' + a[0])
-        self.falseLabel.setText('Решено неверно:   ' + a[1])
-        self.skipLabel.setText('Пропущено:   ' + a[2])
-
-    def close_window(self):
-        self.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
